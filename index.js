cc2345f6fbed7a561567cb99c4429b90a15b533c
guferut <guferut044@gmail.com>
2024-07-02 18:05:23 +0300

    add middlewere and fix bug

--- index.js
+++ index.js
@@ -1,76 +1,41 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
+
 
 import { registerValidation } from "./validations/auth.js";
 
-import UserModel from "./models/user.js";
+
+import checkAuth from "./utils/checkAuth.js";
+
+import * as UserController from './controllers/UserController.js'
 
 mongoose
-  .connect(
-    "mongodb+srv://sanchobregin:test123@cluster0.hlcldmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => console.log("DB is Ok"))
-  .catch((err) => console.log("DB error ", err));
+    .connect(
+        "mongodb+srv://sanchobregin:test123@cluster0.hlcldmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => console.log("DB is Ok"))
+    .catch((err) => console.log("DB error ", err));
 
 const app = express();
 
 app.use(express.json());
 
+app.post('/auth/login', UserController.login);
+
 app.get("/", (req, res) => {
-  res.send("111 hello wrld");
+    res.send("111 hello wrld");
 });
 
-app.get("/auth/register", (req, res) => {
-  res.send("hello wrld from register page");
-});
 
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
 
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
+app.post("/auth/register", registerValidation, UserController.register);
 
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarURL: req.body.avatarURL,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user.id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({ ...userData, token });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Failed to register",
-    });
-  }
-});
+app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.listen(1924, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log("server good");
+    if (err) {
+        return console.log(err);
+    }
+    console.log("server good");
 });
