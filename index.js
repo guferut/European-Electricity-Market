8ca9fa947425b57d0fdeed308c342ee568ee3568
Voider <sanchobregin@gmail.com>
2024-07-02 15:07:35 +0300

    Added user, password encryption with Bcrypt, error handling, hidden user password from server response

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
+
+import UserModel from "./models/user.js";
 
 mongoose
   .connect(
@@ -20,28 +23,49 @@ app.get("/", (req, res) => {
   res.send("111 hello wrld");
 });
 
-app.post("/auth/register", registerValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+app.get("/auth/register", (req, res) => {
+  res.send("hello wrld from register page");
+});
+
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarURL: req.body.avatarURL,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user.id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to register",
+    });
   }
-
-  res.json({
-    succsess: true,
-  });
-
-  //   console.log(req.body);
-  //   const token = jwt.sign(
-  //     {
-  //       email: req.body.email,
-  //       fullName: "vas9 lox",
-  //     },
-  //     "secret123"
-  //   );
-  //   res.json({
-  //     success: true,
-  //     token,
-  //   });
 });
 
 app.listen(1924, (err) => {
--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,12 @@
 import { body } from "express-validator";
 
 export const registerValidation = [
-  body("email").isEmail(),
-  body("password").isLength({ min: 8 }),
-  body("fullName").isLength({ min: 2 }),
-  body("avatarURL").optional().isURL,
+  body("email", "invalid email format").isEmail(),
+  body("password", "password must contain at least 8 characters").isLength({
+    min: 8,
+  }),
+  body("fullName", "The name must contain at least 2 characters").isLength({
+    min: 2,
+  }),
+  body("avatarURL", "Invalid URL format").optional().isURL(),
 ];
